3f20b2bfa18e61dc857b6a94ddf001496841ccaa
Shazib-Wani <shazib20cs202@newhorizonindia.edu>
2022-07-30 16:23:47 +0530

    java

---
+++ src/shazib/my_main.java
@@ -0,0 +1,30 @@
+package shazib;
+import java.io.PrintWriter;
+	public class my_main {
+		public static void main(String[] args) {
+			String input_file="C:\\testfiles\\11.jpg";
+			String output_file="C:\\testfiles\\11";	
+			String tesseract_install_path="C:\\Tesseract-OCR\\tesseract";
+			String[] command =
+		    {
+		        "cmd",
+		    };
+		    Process p;
+			try {
+				p = Runtime.getRuntime().exec(command);
+			        new Thread(new SyncPipe(p.getErrorStream(), System.err)).start();
+		                new Thread(new SyncPipe(p.getInputStream(), System.out)).start();
+		                PrintWriter stdin = new PrintWriter(p.getOutputStream());
+		                stdin.println("\""+tesseract_install_path+"\" \""+input_file+"\" \""+output_file+"\" -l eng");
+		        	    stdin.close();
+		                p.waitFor();
+		                System.out.println();
+		                System.out.println();
+		                System.out.println();
+		                System.out.println();
+		                System.out.println(Read_File.read_a_file(output_file+".txt"));
+		    	} catch (Exception e) {
+		 		e.printStackTrace();
+			}
+		}	
+	}	
